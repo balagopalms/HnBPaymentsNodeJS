b1e05613777c8b55311b132e088dfb5624a64ef2
Balagopal_M <Balagopal_M@infosys.com>
2018-09-07 16:30:40 +0100

    Moved to configs

--- routes/getPaymentSession.js
+++ routes/getPaymentSession.js
@@ -1,27 +1,34 @@
 const request = require('request');
+const config = require('../config/config.json')
 
 exports.getPaymentSession = function(req, res){
 	
-	console.log("Input : "+JSON.stringify(req.body));
+	console.log("Input received : "+JSON.stringify(req.body));
 	var jsonToAdyen = req.body;
-	jsonToAdyen.reference = "HnB100011";
-	jsonToAdyen.shopperReference = "yourShopperId_IOfW3k9G2PvXFu2j";
-	jsonToAdyen.channel ="Web";
-	jsonToAdyen.html = "true";
-	jsonToAdyen.merchantAccount = "HnBECommTest";
-	jsonToAdyen.sdkVersion = "1.3.2";
+	jsonToAdyen.reference = config.adyen.reference;
+	jsonToAdyen.shopperReference = config.adyen.shopperReference;
+	jsonToAdyen.channel = config.adyen.channel;
+	jsonToAdyen.html = config.adyen.html;
+	jsonToAdyen.merchantAccount = config.adyen.merchantAccount;
+	jsonToAdyen.sdkVersion = config.adyen.sdkVersion;
 	console.log("Formatted input : "+JSON.stringify(req.body));
+	var output = 'Test';
 	
 	 request({
-		 url: 'https://checkout-test.adyen.com/v32/paymentSession',
+		 url: config.adyen.sessionEndpoint,
 		 headers : {
-			 'X-API-Key' : 'AQEfhmfuXNWTK0Qc+iSesFABe1h8Cxd32qLHGNBZ+MzNyhDBXVsNvuR83LVYjEgiTGAH-+JQpLkHSDmliqvoYb33/PwvkQ8anUgghE9YykxS+Rlk=-GLKj7HYgxLgjN2d5',
+			 'X-API-Key' : config.adyen.xAPIKey,
 			 'Content-Type' : 'application/json'
 		 },
 		 method: 'POST',	
 		 json: jsonToAdyen
-	 }, function (err, res, body) {
-		 console.log(JSON.stringify(body));
+	 }, function (err2, res2, body2) {
+		 console.log("Resp from Adyen : "+JSON.stringify(body2));
+		 output = JSON.stringify(body2);
+		 res.writeHead(200, {
+			    'Content-Type': 'application/json',
+			});
+			 res.write(output);
+			 res.end();		 
 	  });
-	 res.render('index', { title: 'Nadannu' });
 };
